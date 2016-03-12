5aad98a6861fce67dd3852b510864b245df1396e
hkhoi <hkhoi@outlook.com>
2016-03-12 07:32:50 +0700

    LaplaceBigramLanguageModel.py implemented

--- LaplaceBigramLanguageModel.py
+++ LaplaceBigramLanguageModel.py
@@ -1,20 +1,32 @@
+import math, collections
+
 class LaplaceBigramLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def __init__(self, corpus):
+      self.key_map = collections.defaultdict(lambda: 0)
+      self.bi_key_map = collections.defaultdict(lambda: 0)
+      self.train(corpus)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+    def train(self, corpus):
+      """Takes a HolbrookCorpus corpus, does whatever training is needed."""
+      for sentence in corpus.corpus:
+        previous_token = ''  
+        for datum in sentence.data:  
+          token = datum.word
+          self.key_map[token] = self.key_map[token] + 1
+          if (previous_token):
+              bigram = previous_token + '|' + token
+              self.bi_key_map[bigram] = self.bi_key_map[bigram] + 1
+          previous_token = token
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+    def score(self, sentence):
+      """Takes a list of strings, returns a score of that sentence."""
+      score = 0.0
+      previous_token = '' 
+      for token in sentence:
+        if previous_token:
+            bigram = previous_token + '|' + token  
+            score += math.log(self.bi_key_map[bigram] + 1)
+            score -= math.log(self.key_map[previous_token]+len(self.key_map))
+        previous_token = token    
+      return score
